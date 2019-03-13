1afbd0ef3979324ec4c1d907033a78fd653c09f2
palavicinobsale <cpalavicino@imaginex.cl>
2019-03-13 14:47:45 -0300

    Cart refactoring

--- src/Cart.js
+++ src/Cart.js
@@ -5,14 +5,18 @@ export default class Cart {
   constructor() {
     this.items = []
     this.created = null
-    this.legacyCart = null
-    this.create()
+    this.legacy = null
+    this.get()
   }
 
-  create() {
+  get() {
     fetch('/cart/get_total_cart/')
+      .then(res => res.json())
       .then(data => {
-        this.legacyCart = data
+        this.items = []
+        this.created = Date.now()
+
+        this.legacy = data
       })
       .catch(error => {
         console.log(error.message)
@@ -24,57 +28,48 @@ export default class Cart {
       const { id, quantity } = product
       return fetch(`/product/create/${id}?q=${quantity}`)
         .then(res => res.json())
-        .then(data => {
-          if (data.success) {
+        .then(({ success, status, message, total }) => {
+          if (status === 'ok') {
             this.items = [
               ...this.items,
               product
             ]
-            return data
+            return this
           }
           throw new Error('D:')
         })
     }
-    else {
-      console.warn('product debe ser una instancia de Bsale.Product')
-    }
+    return Promise.reject('product debe ser una instancia de Bsale.Product')
   }
 
-  update(product) {
-    if (product instanceof Product) {
-      const { id, quantity } = product
-      return fetch(`/cart/update_detail/${id}?q=${quantity}`)
-        .then(res => res.json())
-        .then(data => {
-          if (data.success) {
-            // update local state
-            this.items = this.items.map(item => item)
-            return data
+  update(productId, newQuantity) {
+    const [{ id }] = this.items.filter(product => product.id === productId)
+    return fetch(`/cart/update_detail/${id}?q=${newQuantity}`)
+      .then(res => res.json())
+      .then(data => {
+        // if (data.success) {
+        this.items = this.items.map(product => {
+          if (product.id === id) {
+            return new Bsale.Product(id, newQuantity)
           }
-          throw new Error('D:')
+          return product
         })
-    }
-    else {
-      console.warn('product debe ser una instancia de Bsale.Product')
-    }
+        return data
+        // }
+        // throw new Error('D:')
+      })
   }
 
-  remove(product) {
-    if (product instanceof Product) {
-      const { id } = product
-      return fetch(`/cart/delete_detail/${id}`)
-        .then(res => res.json())
-        .then(data => {
-          if (data.success) {
-            // update local state
-            this.items = this.items.filter(item => item)
-            return data
-          }
-          throw new Error('D:')
-        })
-    }
-    else {
-      console.warn('product debe ser una instancia de Bsale.Product')
-    }
+  remove(productId) {
+    const [{ id }] = this.items.filter(product => product.id === productId)
+    return fetch(`/cart/delete_detail/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        // if (data.success) {
+        this.items = this.items.filter(product => product)
+        return data
+        // }
+        // throw new Error('D:')
+      })
   }
 }
